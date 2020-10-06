29cd73fc713c2337c21c26a690be96e1f1ae717f
ninety-four-xychen <94xychen@gmail.com>
2020-10-06 23:06:32 +0800

    Output tester information to standerr.

--- lib/speed_tester.rb
+++ lib/speed_tester.rb
@@ -8,7 +8,7 @@ module SpeedTester
   TEST_RESULT_DEST = '/tmp/result.ini'
 
   def start_test
-    `yes | #{SPEEDTESTER} /u #{url} /g ' ' && mv #{SPEEDTESTER_PATH}/results/*.log #{TEST_RESULT_DEST}`
+    `yes | #{SPEEDTESTER} /u #{url} /g ' ' 1>&2 && mv #{SPEEDTESTER_PATH}/results/*.log #{TEST_RESULT_DEST}`
   end
 
   def parse_result

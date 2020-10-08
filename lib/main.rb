61c73f4cb932cc110d66761fe977045e20144d6e
ninety-four-xychen <94xychen@gmail.com>
2020-10-08 23:23:22 +0800

    Exit if no nodes available

--- lib/main.rb
+++ lib/main.rb
@@ -6,6 +6,8 @@ Clamp do
   parameter 'SUBSCRIPTION_URL', 'The Url of v2ray subscription'
 
   def execute
-    puts V2rayConfig.new(Subscription.new(subscription_url).available_nodes).render
+    available_nodes = Subscription.new(subscription_url).available_nodes
+    exit 1 if available_nodes.empty?
+    puts V2rayConfig.new(available_nodes).render
   end
 end
